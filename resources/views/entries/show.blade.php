83940a23f03ae687b46a844d608e498bd865f649
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-11-25 13:00:07 +0900

    Apply layouts to existing pages

--- resources/views/entries/show.blade.php
+++ resources/views/entries/show.blade.php
@@ -1,11 +1,16 @@
+@extends('layouts.app')
 
-<h1>{{ $entry->title }}</h1>
-<h2>{{ $entry->created_at }}</h2>
+@section('title', $entry->title)
 
-<div>
-{{ $entry->body }}
-</div>
+@section('content')
+    <h1>{{ $entry->title }}</h1>
+    <h2>{{ $entry->created_at }}</h2>
 
-<a href="{{ route('home') }}">
-< Back
-</a>
+    <div>
+    {{ $entry->body }}
+    </div>
+
+    <a href="{{ route('home') }}">
+    < Back
+    </a>
+@endsection
