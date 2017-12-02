7308bd488770352d8a02e2e3605be077d8254bc1
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-12-02 17:03:40 +0900

    Use versioning assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ let mix = require('laravel-mix');
 mix
 .js('resources/assets/js/app.js', 'public/js')
 .copy('node_modules/applicationinsights-js/dist/ai.js', 'public/js/ai.js')
-.sass('resources/assets/sass/app.scss', 'public/css');
+.sass('resources/assets/sass/app.scss', 'public/css')
+.version()
