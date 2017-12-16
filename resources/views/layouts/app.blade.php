4d3eb1d70f78749947aa57ef602b8f6c477cee7d
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-12-16 13:40:39 +0900

    Adjust layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,14 @@
                 @endforeach
             @endif
 
-            @yield('content')
+            <div class="row">
+                <div class="col-md-8">
+                    @yield('content')
+                </div>
+                <div class="cols-md-8">
+                    @yield('sidebar')
+                </div>
+            </div>
         </div>
     </body>
 </html>
