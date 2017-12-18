337bdbd5d9de5c2a26e77fe547904b316488ba6d
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2017-12-18 21:01:30 +0900

    Render entriy's body as markdown

--- app/Entry.php
+++ app/Entry.php
@@ -3,12 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Parsedown;
 
 class Entry extends Model
 {
     public static function getRecentEntries()
     {
         return self::orderBy('created_at', 'desc')->take(7);
+    }
+
+    public function renderBody()
+    {
+        return Parsedown::instance()
+                ->setBreaksEnabled(true)
+                ->setUrlsLinked(true)
+                ->text($this->body);
     }
 
     public function comments()
