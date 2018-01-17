b2784e20502a72c033aca569ad37cf2c159ee992
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2018-01-18 00:35:28 +0900

    Update nad create for installing github auth

---
+++ database/migrations/2018_01_17_152322_create_social_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->string('provider_name')->nullable();
+            $table->string('provider_id')->nullable();
+            $table->timestamps();
+
+            $table->unique('provider_name', 'provider_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+}
